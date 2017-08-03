b4dcae2a1985fe79d10580e6304343d5a7f51637
Ronny Steinbrenner <r.steinbrenner@reply.de>
2017-08-03 13:27:26 +0200

    fix

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 def call(String filename, String user, String server, String port, String deploymentPath = '~/deployment/') {
     httpRequest url: "http://${server}:${port}/shutdown", httpMode: 'POST', validResponseCodes: '200,408,404'
-	sh "ssh ${server}@${port} \"mkdir -p ${deploymentPath}\""
+	sh "ssh ${user}@${server} \"mkdir -p ${deploymentPath}\""
 	sh "scp build/libs/${filename} ${user}@${server}:${deploymentPath}"
 	sh "ssh ${user}@${server} \"nohup java -jar ${deploymentPath}/${filename} --server.port=${port}\" &"
 }
