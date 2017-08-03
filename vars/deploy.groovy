f85e233506c21655557f90b079d653848d9f50cb
Matthias Drews <matthias.drews@comsysto.com>
2017-08-03 12:06:30 +0200

    fix deployment syntax

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,5 @@
 def call(String filename, String user, String server, String port) {
     httpRequest url: "http://${server}:${port}/shutdown", httpMode: 'POST', validResponseCodes: '200,408'
 	sh "scp build/libs/${filename} ${user}@${host}:~/deployment"
-	sh '"ssh ${user}@${host} \"nohup java -jar deployment/${filename} --server.port=${port}\" &"
+	sh "ssh ${user}@${host} \"nohup java -jar deployment/${filename} --server.port=${port}\" &"
 }
